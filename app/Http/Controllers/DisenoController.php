50d525b8b5bbb970358d2f1a2d2e70ba30595dd9
Valentina Barrera Lagos <valentina.barrera1601@alumnos.ubiobio.cl>
2020-08-04 16:23:46 -0400

    configuración de registro de imagenes por medio de los WS

--- app/Http/Controllers/DisenoController.php
+++ app/Http/Controllers/DisenoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Diseno;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class DisenoController extends Controller
@@ -49,44 +51,87 @@ class DisenoController extends Controller
     }
 
     Public function crear(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'bail|required|string',
+            'image' => 'required|file|mimes:jpeg,bmp,png,jpg'
+        ]);
+
+        if ($validator->fails()) {
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'error';
+            $resp->message = 'No cumple con las precondiciones de los campos';
+            $resp->errors = $validator->errors();
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
+        }
+
         $diseno = new Diseno();
         $diseno->nombre = $request->nombre;
-        $diseno->path = $request->path;
+        $path = $request->image->store('public/diseno');
+        $path = str_replace("public/", "storage/", $path);
+        $diseno->path = $path;
         $diseno->save();
-        $data=[
-            'code'=>200,
-            'status'=> 'success',
-            'diseño'=>$diseno];
-        return response()->json($data);
+        $resp = new \stdClass();
+        $resp->code = 200;
+        $resp->status = 'success';
+        $resp->message = 'Diseño registrado con éxito.';
+        $resp->data = $diseno;
+        return response(json_encode($resp), 200)
+            ->header('Content-Type', 'application/json');
     }
 
     public function editar(Request $request){
 
-        $result = new \stdClass();
-        $result->code = 200;
+        $validator = Validator::make($request->all(), [
+            'id' => 'bail|numeric|required',
+            'nombre' => 'nullable|string',
+            'image' => 'nullable|file|mimes:jpeg,bmp,png,jpg'
+        ]);
 
-        if($request->id == ''){
-            $result->code=400;
-            $result->message = "Debes seleccionar un id de diseño para editar";
-            return response()->json($result);
+        if ($validator->fails()) {
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'error';
+            $resp->message = 'No cumple con las precondiciones de los campos';
+            $resp->errors = $validator->errors();
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
         }
 
         try{
             $diseno = Diseno::findOrFail($request->id);
-            $diseno->nombre = $request->nombre;
-            $diseno->path = $request->path;
+
+            if(!isset($request->nombre)){
+                $diseno->nombre = $request->nombre;
+            }
+
+            if(!isset($request->image)){
+                $path = $request->image->store('public/diseno');
+                $path = str_replace("public/", "storage/", $path);
+                $diseno->path = $path;
+            }
+
             $diseno->save();
-            $result->code =200;
-            $result->status='success';
-            $result->diseno=$diseno;
+
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'success';
+            $resp->message = 'Diseño actualizado con éxito.';
+            $resp->data = $diseno;
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
 
         }catch(ModelNotFoundException $e){
-            $result->code =400;
-            $result->status='error';
-            $result->message='No se encontro el id de diseño';
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'error';
+            $resp->message = 'Diseño no encontrado';
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
         }
 
-        return response()->json($result);
     }
 
     public function eliminar(Request $request){
--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use App\Helpers\Role;
 use Illuminate\Http\Request;
 use App\Models\Producto;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use DateTime;
 
@@ -50,56 +52,121 @@ class ProductoController extends Controller
     Public function crear(Request $request){
 
         $token = $request->header('Authorization');
+
         $role= new Role();
         if($role->admin($token)){
+            $validator = Validator::make($request->all(), [
+                'nombre' => 'bail|required|string',
+                'precio' => 'bail|required|numeric',
+                'stock' => 'bail|required|numeric',
+                'Categoria_id' => 'bail|required|numeric',
+                'Marca_id' => 'bail|required|numeric',
+                'img' => 'required|file|mimes:jpeg,bmp,png,jpg'
+            ]);
+
+            if ($validator->fails()) {
+                $resp = new \stdClass();
+                $resp->code = 200;
+                $resp->status = 'error';
+                $resp->message = 'No cumple con las precondiciones de los campos';
+                $resp->errors = $validator->errors();
+                return response(json_encode($resp), 200)
+                    ->header('Content-Type', 'application/json');
+            }
+
             $producto = new Producto();
             $producto->nombre = $request->nombre;
             $producto->precio=$request->precio;
             $producto->stock=$request->stock;
-            $producto->img=$request->img;
+            $path = $request->img->store('public/producto');
+            $path = str_replace("public/", "storage/", $path);
+            $producto->img = $path;
             //$producto->TipoProducto_id=$request->TipoProducto_id;
             $producto->Categoria_id=$request->Categoria_id;
             $producto->Marca_id=$request->Marca_id;
             $producto->save();
-            $data=[
-                'code'=>200,
-                'status'=> 'success',
-                'Producto'=>$producto];
-            return response()->json($data);
+
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'success';
+            $resp->message = 'Producto registrado con éxito.';
+            $resp->data = $producto;
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
         }
-        $data=[
-            'code'=>400,
-            'status'=> 'error',
-            'mensaje'=>'el usuario no es administrador'];
-        return response()->json($data);
+
+        $resp = new \stdClass();
+        $resp->code = 400;
+        $resp->status = 'error';
+        $resp->message = 'No tiene autorización para realizar esto.';
+        return response(json_encode($resp), 200)
+            ->header('Content-Type', 'application/json');
     }
 
     public function editar(Request $request){
 
-        $result = new \stdClass();
-        $result->code = 200;
+        $validator = Validator::make($request->all(), [
+            'id' => 'bail|required|numeric',
+            'nombre' => 'bail|nullable|string',
+            'precio' => 'bail|nullable|numeric',
+            'stock' => 'bail|nullable|numeric',
+            'Categoria_id' => 'bail|nullable|numeric',
+            'Marca_id' => 'bail|nullable|numeric',
+            'img' => 'nullable|file|mimes:jpeg,bmp,png,jpg'
+        ]);
 
-        if($request->id == ''){
-            $result->code=400;
-            $result->message = "Debes seleccionar un id del producto para editar";
-            return response()->json($result);
+        if ($validator->fails()) {
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'error';
+            $resp->message = 'No cumple con las precondiciones de los campos';
+            $resp->errors = $validator->errors();
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
         }
 
         try{
             $producto = Producto::findOrFail($request->id);
-            $producto->nombre = $request->nombre;
+            if(isset($request->nombre)){
+                $producto->nombre = $request->nombre;
+            }
+            if(isset($request->precio)){
+                $producto->precio = $request->precio;
+            }
+            if(isset($request->stock)){
+                $producto->stock = $request->stock;
+            }
+            if(isset($request->Categoria_id)){
+                $producto->Categoria_id = $request->Categoria_id;
+            }
+            if(isset($request->Marca_id)){
+                $producto->Marca_id = $request->Marca_id;
+            }
+            if(isset($request->img)){
+                $path = $request->img->store('public/producto');
+                $path = str_replace("public/", "storage/", $path);
+                $producto->img = $path;
+            }
+
             $producto->save();
-            $result->code = 200;
-            $result->status='success';
-            $result->producto=$producto;
+
+            $resp = new \stdClass();
+            $resp->code = 200;
+            $resp->status = 'success';
+            $resp->message = 'Diseño actualizado con éxito.';
+            $resp->data = $producto;
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
 
         }catch(ModelNotFoundException $e){
-            $result->code =400;
-            $result->status='error';
-            $result->message='No se encontro el id del  producto';
+            $resp = new \stdClass();
+            $resp->code = 400;
+            $resp->status = 'error';
+            $resp->message = 'Producto no encontrado';
+            return response(json_encode($resp), 200)
+                ->header('Content-Type', 'application/json');
         }
 
-        return response()->json($result);
     }
 
     public function eliminar(Request $request){
