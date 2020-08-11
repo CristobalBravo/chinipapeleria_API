35b49c3ea7f8207aeb1bf2b6ae7e5e853250ea9c
FrancoFIRMS <francofirms1@gmail.com>
2020-08-11 01:46:15 -0400

    -CRUD Cuaderno -CRUD ConfiguracionCuaderno -Implementacion controllers Cuaderno y ConfCuaderno -Definicion de rutas -Creacion de modelos y relaciones Cuaderno y ConfCuaderno

---
+++ app/Http/Controllers/CuadernoController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cuaderno;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Validator;
+
+class CuadernoController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('api.auth');
+    }
+
+    public function all(){
+        $cuaderno = Cuaderno::all()->load('producto');
+        $data=[
+            'code'=> 200,
+            'status'=>'success',
+            'cuaderno'=>$cuaderno
+        ];
+        return response()->json($data);
+    }
+
+    public function buscarPorID(Request $request){
+
+        $result = new \stdClass();
+        $result->code = 200;
+
+        if($request->id == ''){
+            $result->code=400;
+            $result->status='error';
+            $result->message = "Debes seleccionar un id de cuaderno para buscar";
+            return response()->json($result);
+        }
+
+        try{
+            $id = $request->id;
+            $cuaderno = Cuaderno::findOrFail($id)->load('producto');
+            $result->code = 200;
+            $result->status='success';
+            $result->cuaderno=$cuaderno;
+        }catch(ModelNotFoundException $e){
+            $result->code =400;
+            $result->status='error';
+            $result->message='No se encontro el id';
+        }
+
+        return response()->json($result);
+    }
+
+    Public function crear(Request $request){
+
+        if(!empty($request->all())){
+            $validate = Validator::make($request->all(),[
+                'Producto_id'=>'required']);
+
+            if($validate->fails()){
+                $data=[
+                    'code'=>400,
+                    'status'=> 'error',
+                    'mensaje'=>'La cuaderno no ha podido crearse',
+                    'errores'=>$validate->errors()];
+            }else{
+                
+                $cuaderno = new Cuaderno();
+                $cuaderno->Producto_id=$request->Producto_id;
+                $cuaderno->save();
+                $data=[
+                    'code'=>200,
+                    'status'=> 'success',
+                    'cuaderno'=>$cuaderno];
+            }
+        }else{
+            $data=[
+                'code'=>200,
+                'status'=> 'error',
+                'mensaje'=>'No ha ingresado los parametros correcto'];
+        }
+
+        return response()->json($data);
+    }
+
+    public function editar(Request $request){
+
+        $result = new \stdClass();
+        $result->code = 200;
+
+        if($request->id == ''){
+            $result->code=400;
+            $result->message = "Debes seleccionar un id del cuaderno para editar";
+            return response()->json($result);
+        }
+
+        try{
+            $id = $request->id;
+            $cuaderno = Cuaderno::findOrFail($id)->load('producto');
+            $cuaderno->Producto_id=$request->Producto_id;
+            unset($request->id);
+            $cuaderno->save();
+            $result->code = 200;
+            $result->status='success';
+            $result->cuaderno=$cuaderno;
+
+        }catch(ModelNotFoundException $e){
+            $result->code =400;
+            $result->status='error';
+            $result->message='No se encontro el id del cuaderno';
+        }
+
+        return response()->json($result);
+    }
+
+    public function eliminar(Request $request){
+
+        $result = new \stdClass();
+        $result->code = 200;
+
+        if($request->id == ''){
+            $result->code = 400;
+            $result->status='error';
+            $result->message = "Debes seleccionar un id del cuaderno para Eliminar";
+            return response()->json($result);
+        }
+
+        try{
+            $id = $request->id;
+            $cuaderno = Cuaderno::findOrFail($id);
+            $cuaderno->delete();
+            $result->code = 200;
+            $result->status='success';
+            $result->message='Cuaderno Eliminado Exitosamente';
+        }catch(ModelNotFoundException $e){
+            $result->code = 400;
+            $result->status='error';
+            $result->message='No se encontro el id';
+        }
+
+        return response()->json($result);
+    }
+}
